79f08269a194d769cf22e90937268b05ec486983
bebob94 <bebo.macis@gmail.com>
2023-03-15 11:35:09 +0100

    Initial commit

---
+++ src/strutture_di_controllo/whilee.java
@@ -0,0 +1,24 @@
+package strutture_di_controllo;
+
+import java.util.Scanner;
+
+
+public class whilee {
+	Scanner s = new Scanner(System.in);
+	String myString;
+	
+	
+	public void whileee() {
+		do {
+			System.out.println("inserisci una stringa");
+			myString= s.next();
+			String[] letters = myString.split("");
+			System.out.println(String.join(",", letters));
+			
+		}while(myString !="q");
+			
+		
+	}
+	
+}
+
